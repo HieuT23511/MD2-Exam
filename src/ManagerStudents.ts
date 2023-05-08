f3a066476e5c7b82e9c78d9edd86a28b82fadba6
HieuT23511 <hieut23511@gmail.com>
2023-05-08 11:40:16 +0700

    Edit

--- src/ManagerStudents.ts
+++ src/ManagerStudents.ts
@@ -13,7 +13,7 @@ export class ManagerStudents {
             console.log(`No data exist! `);
             return
         }
-        let newListFiveStudents : Student[]=[];
+        let newListFiveStudents: Student[] = [];
         for (let i = 0; i < this.listStudent.length; i++) {
             newListFiveStudents.push(this.listStudent[i])
             if (i === 4) {
@@ -44,47 +44,66 @@ export class ManagerStudents {
             console.log(`------`);
         }
     }
-    inputInformationOfNewStudent():Student |undefined{
-        let classDefault : string[]=['C0223G1','C0223H1','C0223A1','C0223E1'];
+
+    inputInformationOfNewStudent(): Student | undefined {
+        let classDefault: string[] = ['C0223G1', 'C0223H1', 'C0223A1'];
         let idStudent = readlineSync.question(`Enter id:`)
-        if(this.findIndexStudentByID(idStudent)!==-1){
+        if (this.findIndexStudentByID(idStudent) !== -1) {
             console.log(`This is invalid! }`)
             return
         }
         let nameStudent = readlineSync.question(`Enter name: `);
-        let classOfStudent = readlineSync.keyInSelect(classDefault,`Enter class: `);
+        let indexClassOfStudent = readlineSync.keyInSelect(classDefault, `Enter class: `, {cancel: 'C0223E1'});
+        let classOfStudent: string;
+        switch (indexClassOfStudent) {
+            case 0:
+                classOfStudent = classDefault[0];
+                break;
+            case 1:
+                classOfStudent = classDefault[1];
+                break;
+            case 2:
+                classOfStudent = classDefault[2];
+                break;
+            default:
+                classOfStudent = 'C0223E1'
+                break;
+        }
         let addressOfStudent = readlineSync.question(`Enter address: `);
         let pointOfStudent = readlineSync.question(`Enter point:`);
         let hobbyOfStudent = readlineSync.question(`Enter hobby: `);
-        if(nameStudent.length ===0 || classOfStudent.length ===0
-            || addressOfStudent.length ===0 || pointOfStudent.length ===0 || hobbyOfStudent.length ===0){
+        if (nameStudent.length === 0 || classOfStudent.length === 0
+            || addressOfStudent.length === 0 || pointOfStudent.length === 0 || hobbyOfStudent.length === 0) {
             console.log(`Input information is invalid! Please re-enter..`);
             return;
         }
-        return new Student(idStudent,nameStudent,classOfStudent,addressOfStudent,pointOfStudent,hobbyOfStudent);
+        return new Student(idStudent, nameStudent, classOfStudent, addressOfStudent, pointOfStudent, hobbyOfStudent);
     }
-    addNewStudent(){
+
+    addNewStudent() {
         let newStudentNeedAdd = this.inputInformationOfNewStudent();
-        if (newStudentNeedAdd){
+        if (newStudentNeedAdd) {
             this.listStudent.push(newStudentNeedAdd);
         }
         console.log(`Add new student successful! `)
 
     }
-    findIndexStudentByID(id:string){
+
+    findIndexStudentByID(id: string) {
 
         return this.listStudent.findIndex(elements => id === elements.getIdStudent());
     }
-    editInfoOfStudent(){
+
+    editInfoOfStudent() {
         let idStudent = readlineSync.question(`Enter ID need to find: `);
-        if(this.findIndexStudentByID(idStudent)===-1){
+        if (this.findIndexStudentByID(idStudent) === -1) {
             console.log(`This ID is invalid! `)
             return
         }
         let index = this.findIndexStudentByID(idStudent);
         if (index !== -1) {
             let updateStudent = this.inputInformationOfNewStudent();
-            if(updateStudent){
+            if (updateStudent) {
                 this.listStudent[index].setNameStudent(updateStudent.getNameStudent());
                 this.listStudent[index].setClassStudent(updateStudent.getClassStudent());
                 this.listStudent[index].setAddress(updateStudent.getAddress());
@@ -94,10 +113,11 @@ export class ManagerStudents {
             console.log('Edit Information of Student successfully!');
         }
     }
-    deleteStudentByID(){
+
+    deleteStudentByID() {
         let idStudent = readlineSync.question(`Enter ID need to find: `);
         let index = this.findIndexStudentByID(idStudent);
-        if(index===-1){
+        if (index === -1) {
             console.log(`Not existed Student need to delete!`);
             return;
         }
